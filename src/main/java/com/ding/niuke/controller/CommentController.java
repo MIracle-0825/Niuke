d54457644b354751f0390de9845005ee9f96ff38
Miracle <18435182312@163.com>
2023-02-16 10:50:02 +0800

    添加评论

---
+++ src/main/java/com/ding/niuke/controller/CommentController.java
@@ -0,0 +1,30 @@
+package com.ding.niuke.controller;
+
+import com.ding.niuke.entity.Comment;
+import com.ding.niuke.service.CommentService;
+import com.ding.niuke.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private HostHolder hostHolder;
+    @PostMapping(value = "/add/{discussPostId}")
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment){
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+        return "redirect:/discuss/detail/"+discussPostId;
+    }
+}
