e2eee32769cdb96a21a76f28a26430f34436680d
Miracle <18435182312@163.com>
2023-02-20 10:33:36 +0800

    关注列表，粉丝列表

--- src/main/java/com/ding/niuke/controller/FollowController.java
+++ src/main/java/com/ding/niuke/controller/FollowController.java
@@ -1,20 +1,31 @@
 package com.ding.niuke.controller;
 
+import com.ding.niuke.entity.Page;
 import com.ding.niuke.entity.User;
 import com.ding.niuke.service.FollowService;
+import com.ding.niuke.service.UserService;
+import com.ding.niuke.util.CommunityConstant;
 import com.ding.niuke.util.CommunityUtils;
 import com.ding.niuke.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
     @Autowired
     private FollowService followService;
     @Autowired
     private HostHolder hostHolder;
+    @Autowired
+    private UserService userService;
     @PostMapping(value = "/follow")
     @ResponseBody
     public String follow(int entityType,int entityId){
@@ -28,5 +39,32 @@ public class FollowController {
         User user = hostHolder.getUser();
         followService.unfollow(user.getId(),entityType,entityId);
         return CommunityUtils.getJSONString(0,"已取消关注");
+    }
+    @GetMapping("/followees/{userId}")
+    public String getFollowees(@PathVariable("userId") int userId,Page page, Model model){
+        User user = userService.findUserById(userId);
+        if(user==null){
+            throw new RuntimeException("该用户不存在！");
+        }
+        model.addAttribute("user",user);
+        page.setLimit(5);
+        page.setPath("/followees/"+userId);
+        page.setRows((int)followService.findFolloweeCount(userId,CommunityConstant.ENTITY_TYPE_USER));
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if(userList!=null){
+            for(Map<String,Object> map:userList){
+                User u = (User)map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/follower";
+    }
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
     }
 }
